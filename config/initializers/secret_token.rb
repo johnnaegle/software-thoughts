4b5679b8ab273fb1593aa31a661e53be4ae91c63
John Naegle <john.naegle@goodmeasures.com>
2014-03-14 21:49:02 -0500

    skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoftwareThoughts::Application.config.secret_key_base = '27e1a19a904a4f2debb979eadbb1bc25289ac8bcc01d51e973c35dbf02609125b2e5ce12f2193bc40f8080710a3a83f634cd697d2de6c547f93c9fb01370eae1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoftwareThoughts::Application.config.session_store :cookie_store, key: '_software-thoughts_session'
